f31a8314dfc98a932bdcd602ed073037b18d1486
Satyam Kumar <ksatyam433@gmail.com>
2024-04-06 15:17:13 +0530

    update: login form ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home";
 import LogIn from "./pages/LogIn";
+
 const App = () => {
     return (
         <BrowserRouter>
--- frontend/src/pages/LogIn.jsx
+++ frontend/src/pages/LogIn.jsx
@@ -1,9 +1,8 @@
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
 const LogIn = () => {
     const [phoneNumber, setPhoneNumber] = useState("");
     const [showErrorMessage, setShowErrorMessage] = useState(false); // State for error message
-    const navigate = useNavigate();
+    const [otpForm, showOtpForm] = useState(false); // State for OTP form [default: false
 
     const handleChange = (event) => {
         setPhoneNumber(event.target.value);
@@ -17,14 +16,14 @@ const LogIn = () => {
             setShowErrorMessage(true); // Show error message on invalid submit
         } else {
             // Proceed with OTP logic
-            navigate("/login-confirm");
+            showOtpForm(true);
 
             console.log("Valid phone number:", phoneNumber);
         }
     };
 
     return (
-        <section className='p-10 flex justify-center items-center w-full h-full'>
+        <section className='px-24 sm:p-10 flex justify-center items-center w-full h-full overflow-hidden'>
             <div className=' h-full flex justify-center'>
                 <div className='  '>
                     {/* logo */}
@@ -32,10 +31,10 @@ const LogIn = () => {
                         <h1 className=' text-4xl'>Anuraksha</h1>
                     </div>
                     {/* form */}
-                    <div className=' flex flex-col gap-y-4 p-2'>
-                        <h2 className=' text-xl text-center'>SignIn</h2>
+                    <div className=' flex flex-col gap-y-4 p-1'>
+                        <h2 className=' text-2xl font-bold text-center'>SignIn</h2>
                         <form onSubmit={handleSubmit}>
-                            <div className=' flex  flex-col gap-y-8 text-lg p-4 border border-primary rounded-xl drop-shadow-lg shadow-md '>
+                            <div className=' flex  flex-col gap-y-8 text-lg p-4 sm:p-4 rounded-xl '>
                                 <div className='flex flex-col text-center'>
                                     <label
                                         htmlFor='email'
@@ -47,7 +46,7 @@ const LogIn = () => {
                                         type='number'
                                         id='email'
                                         name='email'
-                                        className=' rounded-lg p-1 my-1'
+                                        className=' rounded-lg p-2   my-1 w-[80vw] sm:w-[40vw] mx-auto bg-transparent border-b-2 border-primary focus:outline-none focus:border-secondary'
                                         placeholder='Enter your 10 digit Number'
                                         value={phoneNumber}
                                         onChange={handleChange}
@@ -58,12 +57,29 @@ const LogIn = () => {
                                         </p>
                                     )}
                                 </div>
+                                {otpForm && (
+                                    <div className='flex flex-col text-center'>
+                                        <label
+                                            htmlFor='otp'
+                                            className=''
+                                        >
+                                            OTP
+                                        </label>
+                                        <input
+                                            type='number'
+                                            id='otp'
+                                            name='otp'
+                                            className=' rounded-lg p-2 my-1'
+                                            placeholder='Enter OTP'
+                                        />
+                                    </div>
+                                )}
                                 <button
                                     type='submit'
-                                    className=' bg-secondary disabled:bg-gray-300 active:bg-primary p-2 '
+                                    className=' bg-secondary disabled:bg-gray-300 active:bg-primary p-2 rounded-full'
                                     disabled={!isValidNumber} // Disable button if invalid
                                 >
-                                    Get OTP
+                                    {otpForm ? "Verify OTP" : "Get OTP"}
                                 </button>
                             </div>
                         </form>
