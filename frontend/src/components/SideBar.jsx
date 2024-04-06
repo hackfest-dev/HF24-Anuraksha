347e356aef8f9ab8ee90dfdbf5a1312bfa16c75c
Satyam Kumar <ksatyam433@gmail.com>
2024-04-06 16:47:05 +0530

    fix: sidebar icon

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -70,7 +70,20 @@ export default function Sidebar({ showMenu, setShowMenu }) {
                 </ListItem>
                 <ListItem>
                     <ListItemPrefix>
-                        <ShoppingBagIcon className='h-5 w-5 text-red-900' />
+                        <svg
+                            xmlns='http://www.w3.org/2000/svg'
+                            fill='red'
+                            viewBox='0 0 24 24'
+                            stroke-width='1.5'
+                            stroke='currentColor'
+                            class='w-6 h-6'
+                        >
+                            <path
+                                stroke-linecap='round'
+                                stroke-linejoin='round'
+                                d='M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z'
+                            />
+                        </svg>
                     </ListItemPrefix>
                     Volunteer
                 </ListItem>
@@ -88,12 +101,7 @@ export default function Sidebar({ showMenu, setShowMenu }) {
                         />
                     </ListItemSuffix>
                 </ListItem>
-                <ListItem>
-                    <ListItemPrefix>
-                        <UserCircleIcon className='h-5 w-5 text-red-900' />
-                    </ListItemPrefix>
-                    Profile
-                </ListItem>
+
                 <ListItem>
                     <ListItemPrefix>
                         <Cog6ToothIcon className='h-5 w-5 text-red-900' />
