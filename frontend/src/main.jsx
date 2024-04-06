df556f5ae6a58d31872b21f1197cf1010a286697
KCKashish <kashishsheregar@gmail.com>
2024-04-06 23:55:32 +0530

    feat: add language map json files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import i18n from "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <i18n.Provider value={i18n}>
-            <App />
-        </i18n.Provider>
+        <App />
     </React.StrictMode>
 );
