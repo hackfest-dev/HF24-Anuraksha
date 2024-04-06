2e46151e4830744da4ebe401ee779afbac673fc3
Satyam Kumar <ksatyam433@gmail.com>
2024-04-07 00:32:25 +0530

    fix: about page content

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -5,12 +5,20 @@ const About = () => {
     return (
         <section className=''>
             <Navbar />
-            <div className='pt-16'>
+            <div className='pt-16 px-4'>
                 <h1 className='text-2xl font-bold text-center'>About</h1>
-                <p className='text-center'>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Illum deserunt animi
-                    recusandae hic corporis provident modi, vitae quas error maxime ducimus,
-                    inventore libero possimus, dolorum praesentium veritatis! Eaque, amet officia?
+                <p className='py-2'>
+                    <span className='font-bold'>Anuraksha</span> is an emergency SOS application
+                    designed to provide swift assistance during critical situations. With the simple
+                    activation of the SOS command, our app instantly notifies your pre-saved
+                    emergency contacts and nearby volunteers, enabling them to promptly reach out
+                    and offer assistance.
+                </p>
+                <span className='font-bold pt-4'>Our Mission At Anuraksha</span>
+                <p className=''>
+                    , our mission is to ensure the safety and well-being of individuals in times of
+                    crisis. We understand that emergencies can occur unexpectedly, and our app is
+                    designed to provide a reliable solution for seeking help when it matters most.
                 </p>
             </div>
         </section>
