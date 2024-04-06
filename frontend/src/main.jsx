1458c809c5fa0d6201a53244070c7c5e23808561
Satyam Kumar <ksatyam433@gmail.com>
2024-04-06 23:24:58 +0530

    add: language json files

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import i18n from "./i18n";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <i18n.Provider value={i18n}>
+            <App />
+        </i18n.Provider>
     </React.StrictMode>
 );
